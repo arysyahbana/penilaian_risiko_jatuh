1921239ebd93fbe148f18eca837ee4f1063039d0
Ary Syahbana <arysyahbana1@gmail.com>
2025-03-20 10:37:47 +0700

    updt

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,12 +25,12 @@ class AppServiceProvider extends ServiceProvider
     {
         // $settings = Setting::first();
         // View::share('settings', $settings);
-        // if ((bool) env('HTTPS', true)) {
-        //     $this->app['url']->forceScheme('https');
-        // } else {
-        //     $this->app['url']->forceScheme('http');
-        // }
+        if ((bool) env('HTTPS', true)) {
+            $this->app['url']->forceScheme('https');
+        } else {
+            $this->app['url']->forceScheme('http');
+        }
 
-        // $this->app['request']->server->set('HTTPS', env('HTTPS'));
+        $this->app['request']->server->set('HTTPS', env('HTTPS'));
     }
 }
